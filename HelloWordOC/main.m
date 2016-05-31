8c28809de00ab368837bd7cc1fd9e83c727f6f4d
QuestZhang <zs_new@126.com>
2016-05-31 17:46:52 +0800

    OC基础

---
+++ HelloWordOC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWordOC
+//
+//  Created by zhangwenqiang on 16/5/31.
+//  Copyright © 2016年 zhangwenqiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
